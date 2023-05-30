20154678b590058251ac6ae1fbc1b11c58658bad
Christopher Baum <christopherabaum@gmail.com>
2023-05-30 17:11:41 -0500

    Add tests for computeExpectedSwapIn

--- test/test.js
+++ test/test.js
@@ -18,7 +18,7 @@ describe('ComputeExpected', function () {
             assert.equal(computeExpected.computeExpectedSwapOut(swapIn, reserveIn, reserveOut, fee), expectedOut);
         });
 
-        it('should return 2 when swapping 4 in with reserves of 4:4', function () {
+        it('should return 4 when swapping 2 in with reserves of 2:8', function () {
             let computeExpected = new ComputeExpected();
             let swapIn = 2.0;
             let reserveIn = 2.0;
@@ -31,7 +31,27 @@ describe('ComputeExpected', function () {
     });
 
     describe("Test computeExpectedIn without a fee", function () {
-        // TASK 2: Write test cases for computeExpectedSwapIn.
+        it('should return 2 when swapping 4 out with reserves of 4:4', function () {
+            let computeExpected = new ComputeExpected();
+            let swapOut = 2.0;
+            let reserveIn = 4.0;
+            let reserveOut = 4.0;
+            let fee = 0.0;
+            let expectedIn = 4.0;
+
+            assert.equal(computeExpected.computeExpectedSwapIn(swapOut, reserveIn, reserveOut, fee), expectedIn);
+        });
+
+        it('should return 4 when swapping 2 out with reserves of 2:8', function () {
+            let computeExpected = new ComputeExpected();
+            let swapOut = 4.0;
+            let reserveIn = 2.0;
+            let reserveOut = 8.0;
+            let fee = 0.0;
+            let expectedIn = 2.0;
+
+            assert.equal(computeExpected.computeExpectedSwapIn(swapOut, reserveIn, reserveOut, fee), expectedIn);
+        });
     });
 
     describe("Tests with a fee", function () {
